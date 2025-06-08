84a3d4a11d4f3db0cf75c230b1a0219914d3402c
FlyingJ <jason.m.harrington@gmail.com>
2025-06-08 14:36:00 -0400

    Add TextType enum and TextNode class

--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
-print('hello world')
+from textnode import *
 
+def main():
+    tn = TextNode('This is some anchor text', TextType.LINK, 'https://www.boot.dev')
+    print(tn)
+    
+if __name__ == "__main__":
+    main()
