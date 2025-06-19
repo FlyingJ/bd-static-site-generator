5b6e9f67aade2379ba263437f17fdc9a48d2049b
FlyingJ <jason.m.harrington@gmail.com>
2025-06-18 21:23:59 -0400

    Add unit tests for TextNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -7,7 +7,10 @@ class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
-        self.assertEqual(node, node2)
+        self.assertEqual(node, node2)        
+    
+    def test_not_eq(self):
+        pass
 
 
 if __name__ == "__main__":
