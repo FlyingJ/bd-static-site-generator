8ece9930f11f7b9a2c2c61984f0dd82af224f8e4
FlyingJ <jason.m.harrington@gmail.com>
2025-07-09 16:02:24 -0400

    Add third test to test_htmlnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,7 +3,7 @@ import unittest
 from htmlnode import HTMLNode
 
 class TestHTMLNode(unittest.TestCase):
-    def test_props_to_html(self):
+    def test_props_to_html_0(self):
         node = HTMLNode(
             'p',
             'this is some text',
@@ -16,7 +16,7 @@ class TestHTMLNode(unittest.TestCase):
         expected = ' href="https://www.google.com" target="_blank"'
         self.assertEqual(node.props_to_html(), expected)        
     
-    def test_eq_1(self):
+    def test_string_repre(self):
         node = HTMLNode(
             'p',
             'this is some text',
@@ -27,7 +27,22 @@ class TestHTMLNode(unittest.TestCase):
             }
             )
         print(node)
-        pass
+
+    def test_props_to_html_1(self):
+        node = HTMLNode(
+            'a',
+            'this is text',
+            None,
+            {
+                "some": "random words",
+                "being": "used as properties",
+                "href": "https://www.google.com",
+                "target": "_blank",
+            }
+            )
+        expected = ' some="random words" being="used as properties" href="https://www.google.com" target="_blank"'
+        self.assertEqual(node.props_to_html(), expected)
+
 
 if __name__ == "__main__":
     unittest.main()
