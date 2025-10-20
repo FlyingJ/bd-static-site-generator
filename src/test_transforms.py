4ca12a340c7417c5f97914a156b9bdb93b773a4d
FlyingJ <jason.m.harrington@gmail.com>
2025-10-19 23:47:45 -0400

    Add transforms.split_nodes_delimiter() and tests

--- src/test_transforms.py
+++ src/test_transforms.py
@@ -2,9 +2,9 @@ import unittest
 
 from textnode import TextNode
 from texttype import TextType
-from transforms import text_node_to_html_node
+from transforms import split_nodes_delimiter, text_node_to_html_node
 
-class TestTransforms(unittest.TestCase):
+class TestTextToHTMLTransforms(unittest.TestCase):
     def test_text(self):
         node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
@@ -55,6 +55,43 @@ class TestTransforms(unittest.TestCase):
 
     def test_text_to_html_node_error(self):
         pass
+
+class TestTextSplitTransforms(unittest.TestCase):
+    def test_split_nodes_delimiter_text_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        result = split_nodes_delimiter([node], "`", TextType.CODE)
+        expectation = [
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),
+            ],
+        ]
+        self.assertEqual(result, expectation)
+
+    def test_split_nodes_delimiter_text_bold(self):
+        node = TextNode("This is text with a **bold** word", TextType.TEXT)
+        result = split_nodes_delimiter([node], "**", TextType.BOLD)
+        expectation = [
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("bold", TextType.BOLD),
+                TextNode(" word", TextType.TEXT),
+            ],
+        ]
+        self.assertEqual(result, expectation)
+
+    def test_split_nodes_delimiter_text_italic(self):
+        node = TextNode("This is text with an _italicized_ word", TextType.TEXT)
+        result = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        expectation = [
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("italicized", TextType.ITALIC),
+                TextNode(" word", TextType.TEXT),
+            ],
+        ]
+        self.assertEqual(result, expectation)
 
 if __name__ == '__main__':
     unittest.main()
--- src/transforms.py
+++ src/transforms.py
@@ -1,4 +1,22 @@
 from leafnode import LeafNode
+from textnode import TextNode
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    new_type = text_type    
+
+    for old_node in old_nodes:
+        more_newer_nodes = []
+        old_text = old_node.text
+        old_type = old_node.text_type
+        
+        substrings = old_text.split(delimiter)
+        do_new_type = False
+        for substring in substrings:
+            more_newer_nodes.append(TextNode(substring, new_type if do_new_type else old_type))
+            do_new_type = not do_new_type
+        new_nodes.append(more_newer_nodes)
+    return new_nodes
 
 def text_node_to_html_node(text_node):
     conversion_map = {
