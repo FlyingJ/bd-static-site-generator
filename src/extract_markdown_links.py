23dfb1b9b3843e9fdd8258d4bb7dbac60bc631af
FlyingJ <jason.m.harrington@gmail.com>
2025-10-21 12:26:32 -0400

    Move extract functions and tests to own files

---
+++ src/extract_markdown_links.py
@@ -0,0 +1,20 @@
+import unittest
+
+import re
+
+def extract_markdown_links(text):
+    regex = r'[^!]\[([^\]]+)\]\(([^\)]+)\)'
+    return re.findall(regex, text)
+
+class TestExtractMarkdownLinks(unittest.TestCase):
+    def test_extract_markdown_links_0(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        result = extract_markdown_links(text)
+        expectation = [
+            ("to boot dev", "https://www.boot.dev"),
+            ("to youtube", "https://www.youtube.com/@bootdotdev")
+        ]
+        self.assertEqual(result, expectation)
+
+if __name__ == '__main__':
+    unittest.main()
