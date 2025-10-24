b6e4afe40b17c5ed0c34c28f3dbbb23fde5968ea
FlyingJ <jason.m.harrington@gmail.com>
2025-10-23 21:44:42 -0400

    Merge HTMLNode and TextNode definitions with unit test

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,7 @@
+import unittest
+
+from texttype import TextType
+
 class TextNode():
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -15,3 +19,27 @@ class TextNode():
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+class TestTextNode(unittest.TestCase):
+    def test_eq_0(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_eq_1(self):
+        node = TextNode("This is a text node", TextType.BOLD, None)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_neq_0(self):
+        node = TextNode("Some text", TextType.TEXT)
+        node2 = TextNode("Some text", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_neq_1(self):
+        node = TextNode("A text", TextType.TEXT)
+        node2 = TextNode("Another text", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+if __name__ == '__main__':
+    unittest.main()
