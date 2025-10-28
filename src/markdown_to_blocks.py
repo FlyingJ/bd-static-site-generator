8f3849ce466812caeedf8501da1599a059a28950
FlyingJ <jason.m.harrington@gmail.com>
2025-10-28 00:34:41 -0400

    Add markdown_to_blocks and tests

---
+++ src/markdown_to_blocks.py
@@ -0,0 +1,8 @@
+def markdown_to_blocks(markdown):
+	new_blocks = []
+	blocks = markdown.split('\n\n')
+	for block in blocks:
+		block = block.strip()
+		if block != '':
+			new_blocks.append(block)
+	return new_blocks
