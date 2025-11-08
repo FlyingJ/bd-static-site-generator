acbb8bcc77b7bafd8407e2b1c392b080d3d339df
FlyingJ <jason.m.harrington@gmail.com>
2025-11-08 17:22:06 -0500

    Add more tests, find bugs and short-circuit

--- src/blocktype.py
+++ src/blocktype.py
@@ -8,7 +8,45 @@ class BlockType(Enum):
     UNORDERED_LIST = "unordered_list"
     ORDERED_LIST = "ordered_list"
 
+def is_heading(block):
+    return True if block.startswith(('# ','## ', '### ', '#### ', '##### ', '###### ')) else False
+
+def is_code(block):
+    return True if (block.startswith('```') and block.endswith('```')) else False
+
+def is_quote(block):
+    for line in block.splitlines():
+        if not line.startswith('> '):
+            return False
+    return True
+
+def is_unordered_list(block):
+    for line in block.splitlines():
+        if not line.startswith('- '):
+            return False
+    return True
+
+def is_ordered_list(block):
+    for index, line in enumerate(block.splitlines()):
+        if not line.startswith(str(index+1)+'. '):
+            return False
+    return True
 
 def block_to_block_type(markdown):
-	block_type = BlockType.PARAGRAPH
-	return block_type
+    # is it a heading?
+    if is_heading(markdown):
+        return BlockType.HEADING
+    # is it a code block?
+    if is_code(markdown):
+        return BlockType.CODE
+    # perhaps a quote
+    if is_quote(markdown):
+        return BlockType.QUOTE
+    # or an unordered list
+    if is_unordered_list(markdown):
+        return BlockType.UNORDERED_LIST
+    # but the list may be ordered as well
+    if is_ordered_list(markdown):
+        return BlockType.ORDERED_LIST
+    # if we make it here, the block must be plain text
+    return BlockType.PARAGRAPH
--- src/test_blocktype.py
+++ src/test_blocktype.py
@@ -2,12 +2,29 @@ import unittest
 
 from blocktype import BlockType, block_to_block_type
 
-class TestBlock(unittest.TestCase):
+block_to_block_type_test_cases = [
+	('# This is a Heading 1 block', BlockType.HEADING),
+	('## This is a Heading 2 block', BlockType.HEADING),
+	('### This is a Heading 3 block', BlockType.HEADING),
+	('#### This is a Heading 4 block', BlockType.HEADING),
+	('##### This is a Heading 5 block', BlockType.HEADING),
+	('###### This is a Heading 6 block', BlockType.HEADING),
+	('```This is a simple Code block```', BlockType.CODE),
+	('> fdsafdsfdsafds\n> fdafdafds\n> fdafdsafdsfdsafd', BlockType.QUOTE),
+	('- item\n- another item\n- more other item\n- more item', BlockType.UNORDERED_LIST),
+	('1. item 1\n2. item 2\n3. item 3\n4. item 4', BlockType.ORDERED_LIST),
+	('this is some basic text', BlockType.PARAGRAPH),
+	(' ```This is a simple Code block```', BlockType.PARAGRAPH),
+	('>fdsafdsfdsafds\n> fdafdafds\n> fdafdsafdsfdsafd', BlockType.PARAGRAPH),
+	('- item\n- another item\n-more other item\n- more item', BlockType.PARAGRAPH),
+	('1. item 1\n5. item 2\n3. item 3\n4. item 4', BlockType.PARAGRAPH),	
+]
+
+class TestBlockToBlockType(unittest.TestCase):
 	def test_block_to_block_type_0(self):
-		block = '# This is a heading'
-		result = block_to_block_type(block)
-		expectation = BlockType.HEADING
-		self.assertEqual(result, expectation)
+		for block, expectation in block_to_block_type_test_cases: 
+			result = block_to_block_type(block)
+			self.assertEqual(result, expectation)
 
 if __name__ == '__main__':
 	unittest.main()
