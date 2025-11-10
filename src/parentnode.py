48c87c41e00de36f3737e5c40790c3da38c0c95d
FlyingJ <jason.m.harrington@gmail.com>
2025-11-10 16:17:19 -0500

    Fix long line at top of parentnode

--- src/parentnode.py
+++ src/parentnode.py
@@ -3,7 +3,12 @@ from leafnode import LeafNode
 
 class ParentNode(HTMLNode):
     def __init__(self, tag=None, children=None, props=None):
-        super().__init__(tag=tag, value=None, children=children, props=props)
+        super().__init__(
+            tag=tag,
+            value=None,
+            children=children,
+            props=props
+        )
 
     def to_html(self):
         if self.tag is None:
