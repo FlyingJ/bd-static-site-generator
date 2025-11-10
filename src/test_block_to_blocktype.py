595b67c3054d1de9fd3631c9b8a33b9fe6b4274a
FlyingJ <jason.m.harrington@gmail.com>
2025-11-10 16:15:50 -0500

    Split block_to_blocktype out of blocktype, add tests

---
+++ src/test_block_to_blocktype.py
@@ -0,0 +1,31 @@
+import unittest
+
+from block_to_blocktype import block_to_blocktype
+from blocktype import BlockType
+
+block_to_block_type_test_cases = [
+	('# This is a Heading 1 block', BlockType.HEADING),
+	('## This is a Heading 2 block', BlockType.HEADING),
+	('### This is a Heading 3 block', BlockType.HEADING),
+	('#### This is a Heading 4 block', BlockType.HEADING),
+	('##### This is a Heading 5 block', BlockType.HEADING),
+	('###### This is a Heading 6 block', BlockType.HEADING),
+	('```This is a simple Code block```', BlockType.CODE),
+	('> fdsafdsfdsafds\n> fdafdafds\n> fdafdsafdsfdsafd', BlockType.QUOTE),
+	('- item\n- another item\n- more other item\n- more item', BlockType.UNORDERED_LIST),
+	('1. item 1\n2. item 2\n3. item 3\n4. item 4', BlockType.ORDERED_LIST),
+	('this is some basic text', BlockType.PARAGRAPH),
+	(' ```This is a simple Code block```', BlockType.PARAGRAPH),
+	('>fdsafdsfdsafds\n> fdafdafds\n> fdafdsafdsfdsafd', BlockType.PARAGRAPH),
+	('- item\n- another item\n-more other item\n- more item', BlockType.PARAGRAPH),
+	('1. item 1\n5. item 2\n3. item 3\n4. item 4', BlockType.PARAGRAPH),	
+]
+
+class TestBlockToBlockType(unittest.TestCase):
+	def test_block_to_blocktype_0(self):
+		for block, expectation in block_to_block_type_test_cases: 
+			result = block_to_blocktype(block)
+			self.assertEqual(result, expectation)
+
+if __name__ == '__main__':
+	unittest.main()
--- src/test_blocktype.py
+++ src/test_blocktype.py
@@ -1,30 +1,41 @@
 import unittest
 
-from blocktype import BlockType, block_to_block_type
+from blocktype import BlockType
 
-block_to_block_type_test_cases = [
-	('# This is a Heading 1 block', BlockType.HEADING),
-	('## This is a Heading 2 block', BlockType.HEADING),
-	('### This is a Heading 3 block', BlockType.HEADING),
-	('#### This is a Heading 4 block', BlockType.HEADING),
-	('##### This is a Heading 5 block', BlockType.HEADING),
-	('###### This is a Heading 6 block', BlockType.HEADING),
-	('```This is a simple Code block```', BlockType.CODE),
-	('> fdsafdsfdsafds\n> fdafdafds\n> fdafdsafdsfdsafd', BlockType.QUOTE),
-	('- item\n- another item\n- more other item\n- more item', BlockType.UNORDERED_LIST),
-	('1. item 1\n2. item 2\n3. item 3\n4. item 4', BlockType.ORDERED_LIST),
-	('this is some basic text', BlockType.PARAGRAPH),
-	(' ```This is a simple Code block```', BlockType.PARAGRAPH),
-	('>fdsafdsfdsafds\n> fdafdafds\n> fdafdsafdsfdsafd', BlockType.PARAGRAPH),
-	('- item\n- another item\n-more other item\n- more item', BlockType.PARAGRAPH),
-	('1. item 1\n5. item 2\n3. item 3\n4. item 4', BlockType.PARAGRAPH),	
-]
+class TestBlockType(unittest.TestCase):
+    def test_blocktype_paragraph(self):
+        result = BlockType.PARAGRAPH.value
+        expectation = 'paragraph'
+        self.assertEqual(result, expectation)
 
-class TestBlockToBlockType(unittest.TestCase):
-	def test_block_to_block_type_0(self):
-		for block, expectation in block_to_block_type_test_cases: 
-			result = block_to_block_type(block)
-			self.assertEqual(result, expectation)
+    def test_blocktype_heading(self):
+        result = BlockType.HEADING.value
+        expectation = 'heading'
+        self.assertEqual(result, expectation)
+
+    def test_blocktype_code(self):
+        result = BlockType.CODE.value
+        expectation = 'code'
+        self.assertEqual(result, expectation)
+
+    def test_blocktype_quote(self):
+        result = BlockType.QUOTE.value
+        expectation = 'quote'
+        self.assertEqual(result, expectation)
+
+    def test_blocktype_unordered_list(self):
+        result = BlockType.UNORDERED_LIST.value
+        expectation = 'unordered_list'
+        self.assertEqual(result, expectation)
+
+    def test_blocktype_ordered_list(self):
+        result = BlockType.ORDERED_LIST.value
+        expectation = 'ordered_list'
+        self.assertEqual(result, expectation)
+
+    def test_blocktype_error(self):
+        with self.assertRaises(AttributeError):
+            result = BlockType.FOO.value
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
