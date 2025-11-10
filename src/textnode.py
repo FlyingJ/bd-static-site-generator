aa011278eecfd031a0f391dfbf90fb17dab7a296
FlyingJ <jason.m.harrington@gmail.com>
2025-11-10 16:16:46 -0500

    Remove unit tests from textnode.py

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,3 @@
-import unittest
-
 from texttype import TextType
 
 class TextNode():
@@ -19,27 +17,3 @@ class TextNode():
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
-
-class TestTextNode(unittest.TestCase):
-    def test_eq_0(self):
-        node = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a text node", TextType.BOLD)
-        self.assertEqual(node, node2)
-
-    def test_eq_1(self):
-        node = TextNode("This is a text node", TextType.BOLD, None)
-        node2 = TextNode("This is a text node", TextType.BOLD)
-        self.assertEqual(node, node2)
-
-    def test_neq_0(self):
-        node = TextNode("Some text", TextType.TEXT)
-        node2 = TextNode("Some text", TextType.ITALIC)
-        self.assertNotEqual(node, node2)
-
-    def test_neq_1(self):
-        node = TextNode("A text", TextType.TEXT)
-        node2 = TextNode("Another text", TextType.TEXT)
-        self.assertNotEqual(node, node2)
-
-if __name__ == '__main__':
-    unittest.main()
