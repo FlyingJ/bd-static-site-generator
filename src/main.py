a1d37bf616a74fceafd2af13a9b0dd7d65faf86d
FlyingJ <jason.m.harrington@gmail.com>
2025-12-10 22:33:37 -0500

    Update main() to call sysutils.copy_directory_contents

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
+from sysutils import copy_directory_contents
 
 SRC_DIR='static'
 DST_DIR='public'
 
 def main():
-    raise NotImplementedError('main() incomplete')
+    if not copy_directory_contents(SRC_DIR, DST_DIR):
+        raise Exception(f' - {__name__} failed to copy_directory_contents')
 
 if __name__ == "__main__":
     main()
