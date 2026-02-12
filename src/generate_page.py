87dc0fe52ced37f7488b369b37c59b120e12046a
FlyingJ <jason.m.harrington@gmail.com>
2026-02-11 23:38:53 -0500

    Add generate_page() and have main.sh start a web server

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,2 +1,27 @@
+from pathlib import Path
+
+from extract_title import extract_title
+from markdown_to_htmlnode import markdown_to_htmlnode
+
 def generate_page(from_path, template_path, dest_path):
-    raise NotImplemented()
+    # create Path objects from our provided bits and pieces
+    from_obj = Path(from_path)
+    template_obj = Path(template_path)
+    dest_obj = Path(dest_path)
+    # ensure paths exist and are appropriate file types
+    for file in (from_obj, template_obj):
+        if not file.is_file():
+            raise Exception(f'Error: {file} not a file')
+
+    # greeting message
+    print(f'Generating page from {from_path} to {dest_path} using {template_path}')
+
+    markdown = from_obj.read_text()
+    template_html = template_obj.read_text()
+    content = markdown_to_htmlnode(markdown).to_html()
+    title = extract_title(markdown)
+
+    html = template_html.replace('{{ Title }}', title).replace('{{ Content }}', content)
+
+    dest_obj.parent.mkdir(parents=True, exist_ok=True)
+    dest_obj.write_text(html)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
 from sysutils import copy_directory_contents
+from generate_page import generate_page
 
-SRC_DIR='static'
-DST_DIR='public'
+SOURCE = 'static'
+DESTINATION = 'public'
 
 def main():
-    if not copy_directory_contents(SRC_DIR, DST_DIR):
+    if not copy_directory_contents(SOURCE, DESTINATION):
         raise Exception(f' - {__name__} failed to copy_directory_contents')
+
+    from_path = 'content/index.md'
+    template_path = 'template.html'
+    dest_path = 'public/index.html'
+
+    generate_page(from_path, template_path, dest_path)
+
 
 if __name__ == "__main__":
     main()
